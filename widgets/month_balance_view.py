f44af419c8577d57dbefeb432f630706e54cf2a8
ZMV <zmvph79@gmail.com>
2024-12-11 10:54:01 +0300

    corrected MonthBalanceView.get_data and *.set_table(added parameter stop_date)

--- widgets/month_balance_view.py
+++ widgets/month_balance_view.py
@@ -26,11 +26,11 @@ class MonthBalanceView(Controller):
         month = get_current_date('month')
         return QtWidgets.QLabel(self.interface_languages['month'] + ': ' + month)
 
-    def get_data(self, date: str, table_names: list):
-        return unpacking(self.get_last_time_span_values(date, table_names))
+    def get_data(self, start_date: str, table_names: list, stop_date: str):
+        return unpacking(self.get_last_time_span_values(start_date, table_names, stop_date))
 
-    def set_table(self, start_date: str, table_names=['Credit', 'Category_Credit'], name='expense'):
-        ids, dates, values, categories, notes = self.get_data(start_date, table_names)
+    def set_table(self, start_date: str, table_names=['Credit', 'Category_Credit'], name='expense', stop_date=None):
+        ids, dates, values, categories, notes = self.get_data(start_date, table_names, stop_date)
         total_value = get_view_money(self.get_current_expense(start_date)) \
             if name == 'expense' else get_view_money(self.get_current_income(start_date))
         self.table_view = QtWidgets.QTableView(parent=None)
