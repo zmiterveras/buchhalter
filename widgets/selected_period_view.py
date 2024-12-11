58759ab4dba229ed07b9b222f4ea39f38b498197
ZMV <zmvph79@gmail.com>
2024-12-11 10:39:54 +0300

    overload SelectedPeriodView.get_data

--- widgets/selected_period_view.py
+++ widgets/selected_period_view.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from tools.data_unpacking import unpacking
 from widgets.month_balance_view import MonthBalanceView
 
 
@@ -9,3 +10,5 @@ class SelectedPeriodView(MonthBalanceView):
         choose_period_widget = self.choose_time_span()
         self.view_box.addWidget(choose_period_widget)
 
+    def get_data(self, start_date: str, table_names: list, stop_date:str):
+        return unpacking(self.get_span_time_values(start_date, table_names, stop_date))
