647fd851ddb93e2ece3711606c7dcfd77c8b216b
ZMV <zmvph79@gmail.com>
2024-12-11 10:55:29 +0300

    deleted SelectedPeriodView.get_data

--- widgets/selected_period_view.py
+++ widgets/selected_period_view.py
@@ -10,5 +10,3 @@ class SelectedPeriodView(MonthBalanceView):
         choose_period_widget = self.choose_time_span()
         self.view_box.addWidget(choose_period_widget)
 
-    def get_data(self, start_date: str, table_names: list, stop_date:str):
-        return unpacking(self.get_span_time_values(start_date, table_names, stop_date))
