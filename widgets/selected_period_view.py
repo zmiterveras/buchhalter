b91558f1b8829f8bb8368e2ba72f7b0088313e78
ZMV <zmvph79@gmail.com>
2024-12-13 14:44:01 +0300

    corrected SelectedPeriodView.selected_period(added title label)

--- widgets/selected_period_view.py
+++ widgets/selected_period_view.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from PyQt5 import QtWidgets
+
 from tools.data_unpacking import unpacking
 from widgets.month_balance_view import MonthBalanceView
 
@@ -7,6 +9,8 @@ from widgets.month_balance_view import MonthBalanceView
 class SelectedPeriodView(MonthBalanceView):
 
     def selected_period(self):
+        title = QtWidgets.QLabel(self.interface_languages['select_period_table'])
+        self.view_box.addWidget(title)
         choose_period_widget = self.choose_time_span()
         self.view_box.addWidget(choose_period_widget)
 
