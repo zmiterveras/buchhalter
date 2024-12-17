6af07a2ac79140f48b1e9ddfc554eb8dc05e9ebe
ZMV <zmvph79@gmail.com>
2024-12-17 12:05:57 +0300

    added category_selected_period_view.CategorySelectedPeriodView

---
+++ widgets/category_selected_period_view.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from logging import getLogger
+
+from widgets.month_balance_view import MonthBalanceView
+
+logger = getLogger(__name__)
+
+
+class CategorySelectedPeriodView(MonthBalanceView):
+    pass
