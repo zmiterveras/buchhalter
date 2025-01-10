cc0336ea5e6c395ad20dd0bd188b1e0a829b9e0c
ZMV <zmvph79@gmail.com>
2025-01-10 16:46:35 +0300

    added CategorySelectedPeriodView.selected_category()

--- widgets/category_selected_period_view.py
+++ widgets/category_selected_period_view.py
@@ -3,10 +3,17 @@
 
 from logging import getLogger
 
+from PyQt5 import QtWidgets
+
 from widgets.month_balance_view import MonthBalanceView
 
 logger = getLogger(__name__)
 
 
 class CategorySelectedPeriodView(MonthBalanceView):
-    pass
+
+    def selected_category(self):
+        title = QtWidgets.QLabel(self.interface_languages['category'])
+        self.view_box.addWidget(title)
+        choose_category = self.choose_category()
+        self.view_box.addWidget(choose_category)
