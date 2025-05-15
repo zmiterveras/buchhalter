390521f76e65baa64bf33aea0b611f31b7813b76
ZMV <zmvph79@gmail.com>
2025-05-15 11:11:45 +0300

    Corrected SelectedPeriodView.get_total_value

--- widgets/selected_period_view.py
+++ widgets/selected_period_view.py
@@ -3,6 +3,7 @@
 from PyQt5 import QtWidgets
 
 from tools.data_unpacking import unpacking
+from tools.money_parser import get_view_money
 from widgets.month_balance_view import MonthBalanceView
 
 
@@ -13,6 +14,10 @@ class SelectedPeriodView(MonthBalanceView):
         self.view_box.addWidget(title)
         choose_period_widget = self.choose_time_span()
         self.view_box.addWidget(choose_period_widget)
+
+    def get_total_value(self, start_date: str, name: str, stop_date: str) -> str:
+        return  get_view_money(self.get_span_time_total_value(start_date, 'Credit', stop_date)) \
+            if name == 'expense' else get_view_money(self.get_current_income(start_date))
 
     def balance_update(self, current_balance: str):
         """
