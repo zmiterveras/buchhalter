3ba8031b28c52a01cdd6ffbaa8e1dccdd12b9203
ZMV <zmvph79@gmail.com>
2025-11-10 12:50:43 +0300

    added diagram_view.DiagramView

---
+++ widgets/diagram_view.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from PyQt5 import QtWidgets, QtGui
+from logging import getLogger
+from widgets.controller import Controller
+
+
+logger = getLogger(__name__)
+
+
+class DiagramView(Controller):
+    pass
